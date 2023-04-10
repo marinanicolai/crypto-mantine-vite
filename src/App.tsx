f1d5b21cc873749e8156ed87dc5b9b6142a895dd
marina <nicolaimarina@gmail.com>
2023-04-10 11:37:12 -0400

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import geckoLogo from "./assets/coin-gecko.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Crypto App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>table</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Created by Marina Nicolai and powered by{" "}
+        <a href="https://www.coingecko.com/" target="_blank">
+          <img src={geckoLogo} className="logo" alt="CoinGecko logo" />
+          CoinGecko API
+        </a>
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
