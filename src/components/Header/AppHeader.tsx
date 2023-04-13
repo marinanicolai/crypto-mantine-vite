7a5511cf2073f83b1b504d551927d9b444f3bd33
marina <nicolaimarina@gmail.com>
2023-04-13 12:51:03 -0400

    creating the utilityNav component

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { IconSun, IconMoonStars } from "@tabler/icons-react";
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import { UtilityNav } from "./UtilityNav";
+import {
+  ActionIcon,
+  useMantineColorScheme,
+  Container,
+  Grid,
+  rem,
+} from "@mantine/core";
 
 export const AppHeader = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
   return (
-    <ActionIcon
-      variant="outline"
-      color={dark ? "yellow" : "blue"}
-      onClick={() => toggleColorScheme()}
-      title="Toggle color scheme"
-    >
-      {dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
-    </ActionIcon>
+    <>
+      <Container>
+        <Grid>
+          <UtilityNav />
+          <Grid.Col span={1} offset={1} style={{ minHeight: rem(80) }}>
+            <ActionIcon
+              variant="outline"
+              color={dark ? "yellow" : "blue"}
+              onClick={() => toggleColorScheme()}
+              title="Toggle color scheme"
+            >
+              {dark ? (
+                <IconSun size="1.1rem" />
+              ) : (
+                <IconMoonStars size="1.1rem" />
+              )}
+            </ActionIcon>
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </>
   );
 };
---
+++ src/components/Header/UtilityNav.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Grid, rem } from "@mantine/core";
+
+export const UtilityNav = () => {
+  return (
+    <>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+      <Grid.Col span={1} style={{ minHeight: rem(80) }}>
+        1
+      </Grid.Col>
+    </>
+  );
+};
